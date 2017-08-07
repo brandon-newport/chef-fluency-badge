d2e7e55413f2467d822c222e67f58c5853cc0269
Brandon Newport <brandon.newport@life.church>
2017-08-07 20:28:26 +0000

    updating syntax errors.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 if node['platform_family'] == "rhel"
-	package = httpd
+	package = "httpd"
 elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
@@ -16,7 +16,7 @@ package 'apache2' do
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
 
